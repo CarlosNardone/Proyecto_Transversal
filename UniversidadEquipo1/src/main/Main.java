3e01dfb3f5f12b7c63ae5a1e2db837c9c7957d8f
“Carlos <carlos.nardone143@gmail.com>
2023-09-12 20:53:26 -0300

    Empezando con la parte grafica.

--- UniversidadEquipo1/src/main/Main.java
+++ UniversidadEquipo1/src/main/Main.java
@@ -28,20 +28,24 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
      Connection con =  Conexion.getConexion();
-     InscripcionData incs=new InscripcionData();
-     Inscripcion incscarlos = new Inscripcion(8,4,7);
+//     InscripcionData incs=new InscripcionData();
+//     Alumno alum = new Alumno();
+//     alum.setIdAlumno(5);
+     
+//     carlos.getIdAlumno();
+//     Inscripcion incscarlos = new Inscripcion(alum,8,8);
          
 //     Materia matematicas = new Materia(5,"Matematica", 1 ,true);
 //     Materia ingles = new Materia("Ingles", 2 ,true);
 //     Materia informatica = new Materia("Informatica", 4, true);
 //     Materia lengua = new Materia(8,"Lengua", 3, true);
-     MateriaData mat = new MateriaData();
-     for(Materia materia: mat.listarMaterias()){
-         System.out.println("Nombre de la materia: " +materia.getNombre()); 
-         System.out.println("Año de la materia: " +materia.getAnioMateria()); 
-         System.out.println("ID: " +materia.getIdMateria()); 
-         
-     }
+//     MateriaData mat = new MateriaData();
+//     for(Materia materia: mat.listarMaterias()){
+//         System.out.println("Nombre de la materia: " +materia.getNombre()); 
+//         System.out.println("Año de la materia: " +materia.getAnioMateria()); 
+//         System.out.println("ID: " +materia.getIdMateria()); 
+//         
+//     }
 //     mat.guardarMateria(matematicas);
 //     mat.guardarMateria(ingles);
 //     mat.guardarMateria(lengua);
@@ -57,11 +61,12 @@ public class Main {
      
     
 //       Alumno carlos = new Alumno(37577100,"Dome", "Carlos", LocalDate.of(1994, 11, 22), true);
+       Alumno franco = new Alumno(433132332,"Lopez", "Franco", LocalDate.of(2001, 10, 21), true);
 //        Alumno eduardo = new Alumno(5, 567543553, "Rodrigues", "Juan Eduardo", LocalDate.of(1986, 12, 23), true);
 //        Alumno diego = new Alumno (24652571, "Bazzoni","Diego",LocalDate.of(1976, 1, 23), true);
 //        Alumno mauricio = new Alumno (30068183, "Avila", "Mauricio", LocalDate.of(1983,4,10), true);
-//        AlumnoData alu = new AlumnoData();
-//        alu.guardarAlumno(mauricio);
+        AlumnoData alu = new AlumnoData();
+        alu.guardarAlumno(franco);
 //        alu.modificarAlumno(eduardo);
 //        alu.eliminarAlumno(5);
 //       Alumno alumnoEncontrado = alu.buscarAlumno(5);
